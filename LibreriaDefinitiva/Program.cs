26d6ab8922a9abd6d0894044bcb26c25e945a894
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-03 17:15:13 +0200

    Program.cs

--- LibreriaDefinitiva/Program.cs
+++ LibreriaDefinitiva/Program.cs
@@ -1,6 +1,13 @@
+using LibreriaDefinitiva.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,7 +26,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseStaticFiles();
 app.MapControllers();
 
 app.Run();
