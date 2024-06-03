5b92e3ed011e4994ad71871d67faf506990b79cd
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-03 17:28:24 +0200

    sakr babbo

--- LibreriaDefinitiva/Models/Dto/LibroDTO.cs
+++ LibreriaDefinitiva/Models/Dto/LibroDTO.cs
@@ -1,6 +1,16 @@
-﻿namespace LibreriaDefinitiva.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibreriaDefinitiva.Models.Dto
 {
     public class LibroDTO
     {
+        [Required]
+        [MaxLength(13)]
+        [MinLength(10)]
+        public string Isbn { get; set; }
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public string Genere { get; set; }
+        public string Edizione { get; set; } //Einaudi(2019)
     }
 }
--- LibreriaDefinitiva/Models/Request/AddBookRequest.cs
+++ LibreriaDefinitiva/Models/Request/AddBookRequest.cs
@@ -2,5 +2,10 @@
 {
     public class AddBookRequest
     {
+        public string Isbn { get; set; }
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public string Genere { get; set; }
+        public string Edizione { get; set; } //Einaudi(2019)
     }
 }
