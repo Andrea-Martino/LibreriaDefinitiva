c74d6c168887639095dba8563e17801e58ff14e2
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-03 17:48:42 +0200

    cacca

--- LibreriaDefinitiva/Database/ApplicationDbContext.cs
+++ LibreriaDefinitiva/Database/ApplicationDbContext.cs
@@ -1,7 +1,41 @@
-﻿namespace LibreriaDefinitiva.Database
-{
-    public class ApplicationDbContext
-    {
+﻿using LibreriaDefinitiva.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace LibreriaDefinitiva.Database
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Scaffale> Libreria { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var lines = System.IO.File.ReadAllLines("books.csv");
+            List<Libro> allBooks = lines.Skip(1)
+                                .Select(line => line.Split(';'))
+                                .Select(parts => new Libro
+                                {
+                                    Isbn = parts[0],
+                                    Titolo = parts[1],
+                                    Autore = parts[2],
+                                    Genere = parts[3],
+                                    Edizione = parts[4]
+                                })
+                                .ToList();
+            //Aggiungi libri nello scaffale
+            foreach (var book in allBooks)
+            {
+                if(!(Libreria.Any(s => s.GenereId.Equals(book.Genere))))
+                {
+                    Libreria.Add(new Scaffale
+                    {
+                        GenereId = book.Genere,
+                        ScaffaleDiLibri = allBooks.Where(g => g.Genere.Equals(book.Genere)).ToList()
+                    });
+                }
+            }
+            /*modelBuilder.Entity<Libro>().HasData(allBooks.ToArray());*/
+        }
     }
 }
