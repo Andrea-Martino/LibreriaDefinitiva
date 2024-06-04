aff607fcbe51ffd578f4c964ab83cd5e9c1545d5
Sohaib <sohaibsakr.art@gmail.com>
2024-06-04 08:57:41 +0200

    Aggiunta di Controller e sistemazione di DbContext.

--- LibreriaDefinitiva/Controllers/LibroController.cs
+++ LibreriaDefinitiva/Controllers/LibroController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibreriaDefinitiva.Database;
+using LibreriaDefinitiva.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace LibreriaDefinitiva.Controllers
 {
@@ -6,6 +9,67 @@ namespace LibreriaDefinitiva.Controllers
     [ApiController]
     public class LibroController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
 
+        public LibroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = _context.Libreria.SelectMany(s => s.ScaffaleDiLibri).ToList();
+            return Ok(books);
+        }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Libro newBook)
+        {
+            var scaffale = _context.Libreria.FirstOrDefault(s => s.GenereId == newBook.Genere);
+            if (scaffale == null)
+            {
+                scaffale = new Scaffale
+                {
+                    GenereId = newBook.Genere,
+                    ScaffaleDiLibri = new List<Libro>()
+                };
+                _context.Libreria.Add(scaffale);
+            }
+            scaffale.ScaffaleDiLibri.Add(newBook);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetAllBooks), new { isbn = newBook.Isbn }, newBook);
+        }
+
+        [HttpGet("search")]
+        public IActionResult SearchBooks([FromQuery] string query)
+        {
+            var books = _context.Libreria
+                                .SelectMany(s => s.ScaffaleDiLibri)
+                                .Where(b => b.Titolo.Contains(query) || b.Autore.Contains(query))
+                                .ToList();
+            return Ok(books);
+        }
+
+        [HttpDelete("{isbn}")]
+        public IActionResult DeleteBook(string isbn)
+        {
+            var book = _context.Libreria
+                                .SelectMany(s => s.ScaffaleDiLibri)
+                                .FirstOrDefault(b => b.Isbn == isbn);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            var scaffale = _context.Libreria.FirstOrDefault(s => s.ScaffaleDiLibri.Contains(book));
+            if (scaffale != null)
+            {
+                scaffale.ScaffaleDiLibri.Remove(book);
+                _context.SaveChanges();
+            }
+
+            return NoContent();
+        }
     }
 }
